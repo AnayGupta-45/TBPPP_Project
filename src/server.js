329369e6926b592b8ed414488d73b77465ef7baa
AnayGupta-45 <anaygupta173@gmail.com>
2025-01-17 00:56:19 +0530

    Revert "created an ejs file"
    This reverts commit 743ec9ea135c0b2caf6503725d97081abf731a7c.

--- src/server.js
+++ src/server.js
@@ -31,10 +31,6 @@ app.get("/signin", (req, res) => {
   res.render("signin");
 });
 
-app.get('/about',(req,res)=>{
-  res.render('anay');
-})
-
 // Middlewares..
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
