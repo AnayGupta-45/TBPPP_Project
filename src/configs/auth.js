b1dd9331a3f5c23e2b61f811ae13a646d331d433
Palak Sharma <Palakshandilya99@gmail.com>
2025-01-24 22:36:24 +0530

    ALL DONE

---
+++ src/configs/auth.js
@@ -0,0 +1,14 @@
+
+  export function  ensureAuthenticated(req, res, next) {
+      if (req.isAuthenticated()) {
+        return next();
+      }
+      req.flash('error_msg', 'Please log in to view that resource');
+      res.redirect('/signin');
+    }
+   export function forwardAuthenticated(req, res, next) {
+      if (!req.isAuthenticated()) {
+        return next();
+      }
+      res.redirect('/dashboard');      
+    }
---
+++ src/configs/passport.js
@@ -0,0 +1,40 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+
+// Loading the User model in our authentication..
+const User = require('../models/User');
+
+module.exports = function(passport) {
+  passport.use(
+    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+      // Matching the user..
+      User.findOne({
+        email: email
+      }).then(user => {
+        if (!user) {
+          return done(null, false, { message: 'That email is not registered' });
+        }
+
+        // Matching password..
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if (err) throw err;
+          if (isMatch) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: 'Password incorrect' });
+          }
+        });
+      });
+    })
+  );
+
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+};
